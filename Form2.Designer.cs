4238557950f468939df02ba9abf7f02ea8184311
VolfAleksandr <volf.2004@list.ru>
2023-10-26 19:29:06 +0300

    Create new form and elements

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -1,17 +1,17 @@
 ﻿
 namespace FirstCommits
 {
-    partial class Form1
+    partial class Form2
     {
         /// <summary>
-        /// Обязательная переменная конструктора.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Освободить все используемые ресурсы.
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,11 +21,11 @@ namespace FirstCommits
             base.Dispose(disposing);
         }
 
-        #region Код, автоматически созданный конструктором форм Windows
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
@@ -38,7 +38,7 @@ namespace FirstCommits
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(146, 66);
+            this.label1.Location = new System.Drawing.Point(124, 137);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(46, 17);
             this.label1.TabIndex = 0;
@@ -47,7 +47,7 @@ namespace FirstCommits
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(541, 105);
+            this.label2.Location = new System.Drawing.Point(502, 86);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(46, 17);
             this.label2.TabIndex = 1;
@@ -55,23 +55,23 @@ namespace FirstCommits
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(134, 227);
+            this.button1.Location = new System.Drawing.Point(166, 240);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(141, 73);
+            this.button1.Size = new System.Drawing.Size(136, 72);
             this.button1.TabIndex = 2;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(371, 199);
+            this.button2.Location = new System.Drawing.Point(465, 169);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(333, 199);
+            this.button2.Size = new System.Drawing.Size(219, 101);
             this.button2.TabIndex = 3;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -80,8 +80,8 @@ namespace FirstCommits
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "Form2";
+            this.Text = "Form2";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -95,4 +95,3 @@ namespace FirstCommits
         private System.Windows.Forms.Button button2;
     }
 }
-
